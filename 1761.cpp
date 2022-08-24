9c50cdc058f388377c2bd318cad7e51cdce29df3
Julio Cesar Rocha <54746730+julioCROS@users.noreply.github.com>
2022-08-24 07:55:15 -0300

    Update 1761.cpp

--- 1761.cpp
+++ 1761.cpp
@@ -1,19 +1,20 @@
 #include <iostream>
 #include <math.h>   
+#include <iomanip>
 using namespace std;
 
 #define PI 3.141592654
  
 int main() {
-    float A, float B, float C;
-    cin >> A >> B >> C;
+    float A, B, C;
+    while (cin >> A >> B >> C) {
+        float cat_op;
+        cat_op = B * tan(A * PI / 180);
 
-    float cat_op;
-    cat_op = B * tan(A * PI / 180);
+        float altura_arv;
+        altura_arv = (C + cat_op) * 5;
 
-    float altura_arv;
-    altura_arv = C + cat_op;
-
-    cout << altura_arv << endl;
+        cout << fixed << setprecision(2) << altura_arv << endl;
+    }
     return 0;
 }
