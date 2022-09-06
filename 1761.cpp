ea6dc7e34c74e8e9bc882464bdd4cd16b35f8a3d
Julio Cesar Rocha <julio.1009@hotmail.com>
2022-09-05 21:19:01 -0300

    [#] 1761 fix

--- 1761.cpp
+++ 1761.cpp
@@ -6,15 +6,15 @@ using namespace std;
 #define PI 3.141592654
  
 int main() {
-    float A, B, C;
+    long double A, B, C;
     while (cin >> A >> B >> C) {
-        float cat_op;
-        cat_op = B * tan(A * PI / 180);
+        long double cat_op;
+        cat_op = B * tan(A * PI / 180.00);
 
-        float altura_arv;
+        long double altura_arv;
         altura_arv = (C + cat_op) * 5;
-
-        cout << fixed << setprecision(2) << altura_arv << endl;
+        
+        printf("%.2Lf\n", altura_arv);
     }
     return 0;
 }
